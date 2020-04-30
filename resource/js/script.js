44bc266a23584df6c26e4a2e6a9848e8dd1859f5
saad hossain <61236270+saad836@users.noreply.github.com>
2020-04-30 15:30:01 +0600

    Add files via upload

---
+++ resource/js/script.js
@@ -0,0 +1,1 @@
+function openNav(){document.getElementById("myNav").style.width="100%"}function closeNav(){document.getElementById("myNav").style.width="0%"}$(document).ready(function(){$(".js--service-section").waypoint(function(t){"down"==t?$("nav").addClass("sticky"):$("nav").removeClass("sticky")});mixitup(".grid-portfolio-container")});
